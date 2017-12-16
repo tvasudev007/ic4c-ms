ac01348e9fc363aaa956c036203ae8913d33bc75
tvasudev007 <vasduevtadavarty@gmail.com>
2017-12-16 07:54:13 +0100

    Initial commit

---
+++ src/main/java/com/ge/predix/solsvc/boot/dto/IC4CAssetDTO.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package com.ge.predix.solsvc.boot.dto;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class IC4CAssetDTO {
+	
+	private String uri;
+	private String classification;
+	private String parent;
+	private String id;
+	private String complexType;
+	private String description;
+	private Map<String,String> attributes;
+	private Map<String,IC4CAssetParametersDTO> parameters;
+
+	
+	public String getUri() {
+		return uri;
+	}
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+	public String getClassification() {
+		return classification;
+	}
+	public void setClassification(String classification) {
+		this.classification = classification;
+	}
+	public String getParent() {
+		return parent;
+	}
+	public void setParent(String parent) {
+		this.parent = parent;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getComplexType() {
+		return complexType;
+	}
+	public void setComplexType(String complexType) {
+		this.complexType = complexType;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Map<String, String> getAttributes() {
+		return attributes;
+	}
+	public void setAttributes(Map<String, String> attributes) {
+		this.attributes = attributes;
+	}
+	public Map<String, IC4CAssetParametersDTO> getParameters() {
+		return parameters;
+	}
+	public void setParameters(Map<String, IC4CAssetParametersDTO> parameters) {
+		this.parameters = parameters;
+	}
+	
+}
---
+++ src/main/java/com/ge/predix/solsvc/boot/dto/Values.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package com.ge.predix.solsvc.boot.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 10620506
+ *
+ */
+public class Values {
+	
+	
+/*	private List<InnerValues> innerValues = new ArrayList<>();
+	
+	*//**
+	 * @return the eccCustomAssetList
+	 *//*
+	public List<InnerValues> getInnerValues() {
+		return innerValues;
+	}
+	*//**
+	 * @param eccCustomAssetList the eccCustomAssetList to set
+	 *//*
+	public void setInnerValues(List<InnerValues> innerValues) {
+		this.innerValues = innerValues;
+	}*/
+	
+	private long datapoints[]=new long[3];
+	
+	
+	public long[] getDatapoints(){
+		 
+		 return datapoints;
+	 }
+	
+	public void  setDatapoints(long datapoints[]){
+		 
+		this.datapoints = datapoints;
+	 }
+
+	
+	 public long getTimestamp(){
+		 
+		 return datapoints[0];
+	 }
+	 
+	 public long getValue(){
+		 
+		 return datapoints[1];
+	 }
+	 
+	 public long getQuality(){
+		 
+		 return datapoints[2];
+	 }
+
+}
